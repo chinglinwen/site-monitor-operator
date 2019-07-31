0f3c89d390c119da25f2bc2ff75d17bb0c4c7572
wenzhenglin <wenzhenglin@haodai.net>
2019-07-31 18:01:30 +0800

    generated kind

---
+++ pkg/apis/sitemonitor/group.go
@@ -0,0 +1,6 @@
+// Package sitemonitor contains sitemonitor API versions.
+//
+// This file ensures Go source parsers acknowledge the sitemonitor package
+// and any child packages. It can be removed if any other Go source files are
+// added to this package.
+package sitemonitor
---
+++ pkg/apis/sitemonitor/v1alpha1/doc.go
@@ -0,0 +1,4 @@
+// Package v1alpha1 contains API Schema definitions for the sitemonitor v1alpha1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=sitemonitor.haodai.com
+package v1alpha1
